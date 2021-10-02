b90d6497526cb0c25bc195c0d45a3a69d304c752
CorsoOleg <corso441@gmail.com>
2021-10-02 21:13:35 +0300

    ВВОД

---
+++ index.js
@@ -0,0 +1,25 @@
+
+/*for(var i = 0; i < 10; i++)  {
+	console.log("Hellow");
+	console.log("Hellow");
+}*/
+/*	
+function Great()  {
+console.log("Hellow");
+console.log("web");
+console.log("Dev");
+};
+
+*/
+
+/*
+function Add(a, b) {
+ 	console.log(a + b);
+
+ 	return a + b;
+}
+
+var sum = Add(2, 2);*/
+
+var ul = document.querySelectorAll("li");
+ console.log (ul);
